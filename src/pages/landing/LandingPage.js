5b1f7de167ad66683ae958e379d3cc3a0769b4bb
sofiaversoza <sofversoza@gmail.com>
2023-02-07 17:52:00 -0700

    landing page design

--- src/pages/landing/LandingPage.js
+++ src/pages/landing/LandingPage.js
@@ -9,12 +9,31 @@ export default function LandingPage() {
 
   return (
     <div className="landing">
-      <div className="typewriter-logo">
-        <h1>Prompt</h1>
+      <div className="flex-item-1">
+        <div className="logo-cont">
+          <img 
+            src={require("../../assets/PrmptLogo1.png")} 
+            alt="Prompt logo"
+            className="landing-logo" 
+          />
+        </div>
+        <div className="message-cont">
+          <h2 className="firstmess">Expand your <span>creativity</span>{" "} 
+            <span className="ampersand">&</span>
+          </h2>
+          <h2 className="secondmess">Elevate your writing <span>capabilities</span></h2>
+        </div>
       </div>
-      <div className="actions">
+
+      <div className="flex-item-2">
+        <div className="vision">
+          <p>Our vision is to revolutionize the way aspiring writers approach their craft by providing a comprehensive platform for prompt submissions, management, and inspiration.</p>
+        </div>
+      </div>
+
+      <div className="flex-item-3">
         <button onClick={() => setShowLogin(true)}>Login</button>
-        <button onClick={() => setShowSignup(true)}>Sign up</button>
+        <button onClick={() => setShowSignup(true)}>Join prompt</button>
         {showLogin && <Login setShowLogin={setShowLogin} setShowSignup={setShowSignup} />}
         {showSignup && <Signup setShowSignup={setShowSignup} setShowLogin={setShowLogin} />}
       </div>
