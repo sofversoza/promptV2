480922d2bba69f18177fa770af61122c3d1fc42f
sofiaversoza <sofversoza@gmail.com>
2023-03-30 17:45:51 -0700

    Landing page Updated Logos

--- src/pages/landing/LandingPage.js
+++ src/pages/landing/LandingPage.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import "./LandingPage.css"
 import Login from "../login-signup/Login"
 import Signup from "../login-signup/Signup"
@@ -6,6 +6,34 @@ import Signup from "../login-signup/Signup"
 export default function LandingPage() {
   const [showLogin, setShowLogin] = useState(true)
   const [showSignup, setShowSignup] = useState(false)
+  const [logo, setLogo] = useState("Prompt12W.png")
+
+  const logosArr = [
+    "PromptNewWhite.png",
+    "Prompt2.png",
+    "Prompt3W.png",
+    "Prompt4W.png",
+    "Prompt5W.png",
+    "Prompt6W.png",
+    "Prompt7W.png",
+    "Prompt8W.png",
+    "Prompt9W.png",
+    "Prompt10W.png",
+    "Prompt11W.png",
+    "Prompt12W.png",
+    "Prompt13W.png"
+  ]
+
+  const loadRandomLogo = () => {
+    const randomIndex = Math.floor(Math.random() * logosArr.length)
+    const selected = logosArr[randomIndex]
+    setLogo(selected)
+  }
+
+  useEffect(() => {
+    const interval = setInterval(loadRandomLogo, 500)
+    return () => clearInterval(interval)
+  }, [logo])
 
   return (
     <div className="landing">
@@ -13,15 +41,10 @@ export default function LandingPage() {
       <div className="flexbox-1">
         <div className="logo-cont">
           <img 
-            src={require("../../assets/PromptNewWhite.png")} 
+            src={require(`../../assets/logos/${logo}`)} 
             alt="Prompt logo"
             className="landing-logo" 
           />
-        </div>
-
-        <div className="message-cont">
-          <h2>Elevate your writing <span>&</span></h2>
-          <h2>Expand your creativity</h2>
         </div>
       </div>
 
@@ -32,7 +55,6 @@ export default function LandingPage() {
         {showSignup && 
           <Signup setShowSignup={setShowSignup} setShowLogin={setShowLogin}/>
         }
-
 
         {/* <div className="btn-cont">
           <button onClick={() => setShowLogin(true)}>Get started</button>
@@ -45,6 +67,7 @@ export default function LandingPage() {
           }
         </div> */}
       </div>
+      <div className="flexbox-3"></div>
     </div>
   )
 }
